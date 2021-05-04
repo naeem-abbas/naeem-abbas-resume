467abc8eeb6a6687ae7159f049883f9c68412a0d
NAEEM-ABBAS\Dell <n@gmail.com>
2021-05-04 13:45:02 +0500

    Updates

--- assets/custom.js
+++ assets/custom.js
@@ -1,11 +1,13 @@
+var btn=document.getElementById("resumeRef");
+console.log(btn);
 document.addEventListener("click", function(){
    const resume=document.querySelector("#toPdf");
    console.log(resume);
    var opt = {
-    marginBottom:      2,
+    margin:       0,
     filename:     'myfile.pdf',
     image:        { type: 'jpeg', quality: 0.98 },
-    html2canvas:  { scale: 1 },
+    html2canvas:  { scale: 2 },
     jsPDF:        { unit: 'in', format: 'a4', orientation: 'p' }
   };
    html2pdf().from(resume).set(opt).save("file.pdf");
